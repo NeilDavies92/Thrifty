087ef1bb341c654fc0727fbf8eec90742c0515be
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-06-20 08:09:27 +0100

    Add Hello into directory

---
+++ Hello/hello_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	returnCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("Say hello to a specific person", func(t *testing.T) {
+		got := Hello("Neil", "")
+		want := "Hello, Neil"
+
+		if got != want {
+			returnCorrectMessage(t, got, want)
+		}
+	})
+
+	t.Run("Say hello world if empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		if got != want {
+			returnCorrectMessage(t, got, want)
+		}
+	})
+
+	t.Run("In italian", func(t *testing.T) {
+		got := Hello("Neil", "Italian")
+		want := "Ciao, Neil"
+
+		if got != want {
+			returnCorrectMessage(t, got, want)
+		}
+	})
+
+	t.Run("In french", func(t *testing.T) {
+		got := Hello("Neil", "French")
+		want := "Bonjour, Neil"
+
+		if got != want {
+			returnCorrectMessage(t, got, want)
+		}
+	})
+}
