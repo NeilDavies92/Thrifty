6b3df79ca852e2a1579352b6b5782e462335225e
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-06-20 08:36:01 +0100

    write simple addition function

--- Integers/int_test.go
+++ Integers/int_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
-	result = 4
+	result := 4
 
 	if sum != result {
-		t.Errorf("expected '%d' but got '%d'", expected, result)
+		t.Errorf("expected '%d' but got '%d'", result, sum)
 	}
 }
