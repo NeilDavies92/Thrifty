50dc5b01a2eeb93aa835b96bca9df9b4e9abe471
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-06-29 17:29:55 +0100

    rename files

--- Shapes/shapes_test.go
+++ Shapes/shapes_test.go
@@ -31,14 +31,13 @@ func TestArea(t *testing.T) {
 	})
 
 	t.Run("circles", func(t *testing.T) {
-		circle := Circle{12.0, 6.0}
+		circle := Circle{10}
 		got := Area(circle)
-		want := 72.0
+		want := 314.1592653589793
 
 		if got != want {
 			// %.2f = placeholder for 2 decimal place floating int
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
-
 }
