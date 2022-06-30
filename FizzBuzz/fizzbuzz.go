b006079ea17218b876268c2c7e27b5eea3b67f0e
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-06-30 10:50:17 +0100

    update fizzbuzz function

--- FizzBuzz/fizzbuzz.go
+++ FizzBuzz/fizzbuzz.go
@@ -4,12 +4,12 @@ import "strconv"
 
 func FizzBuzz(i int) string {
 
-	if i%3 == 0 {
+	if i%3 == 0 && i%5 == 0 {
+		return "fizzbuzz"
+	} else if i%3 == 0 {
 		return "fizz"
 	} else if i%5 == 0 {
 		return "buzz"
-	} else if i%3 == 0 && i%5 == 0 {
-		return "fizzbuzz"
 	}
 	return strconv.Itoa(i)
 }
