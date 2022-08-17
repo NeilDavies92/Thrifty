11d2a61264466c5e7035c03ba1df46bd120830a2
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-08-17 10:00:03 +0100

    update tests with some prints

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -1,11 +1,16 @@
 package main
 
-type Wallet struct{}
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
 
 func (w Wallet) Deposit(amount int) {
-
+	fmt.Printf("Address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }
