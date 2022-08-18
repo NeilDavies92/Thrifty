cb33ab58445f55a18023a74d3a3a5f6bd9c75850
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-08-18 12:48:47 +0100

    update value in Bitcoin

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
+// Go supports pointers, allowing you to pass references to values
+// and records within your program.
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("Address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
-	return w.balance
+func (w *Wallet) Balance() Bitcoin {
+	return (*w).balance
 }
